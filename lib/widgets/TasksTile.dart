bc297300558ca7aa7aec2a4abe6b0ec5daeb3c03
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-05-27 23:05:40 +0530

    Update README.md

---
+++ lib/widgets/TasksTile.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class TaskTile extends StatelessWidget {
+
+  final bool? isChecked ;
+   final String? taskTitle;
+   final Function checkboxCallback;
+   const TaskTile({Key? key,  this.isChecked , this.taskTitle, required this.checkboxCallback}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        taskTitle!,
+        style: TextStyle(
+          decoration: isChecked! ? TextDecoration.lineThrough : null,
+          fontSize: 20,
+        ),
+      ),
+      trailing: Checkbox(
+          activeColor: Colors.lightBlueAccent,
+          value: isChecked,
+          onChanged: (newValue){
+            checkboxCallback(newValue);
+          },
+    ),
+    );
+  }
+}
+
+
