8a6f5d6adf031d9e841058695bf84fc5daafa38f
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-05-28 11:59:09 +0530

    Update tasks_screen.dart

--- lib/Screens/tasks_screen.dart
+++ lib/Screens/tasks_screen.dart
@@ -8,33 +8,33 @@ class TasksScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () {},
         backgroundColor: Colors.lightBlueAccent,
         child: const Icon(Icons.add),
       ),
       body: Column(
         children: <Widget>[
           Container(
-            padding:
-                const EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            padding: const EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 CircleAvatar(
-                    backgroundColor: Colors.white,
-                    radius: 30.0,
-                    child: Icon(Icons.list),
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: Icon(Icons.list),
                 ),
                 SizedBox(
                   height: 10.0,
                 ),
-                Text('Todoey',
+                Text(
+                  'Todoey',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 50.0,
                     fontWeight: FontWeight.w700,
                   ),
-
                 ),
                 Text(
                   '12 Tasks',
@@ -43,23 +43,56 @@ class TasksScreen extends StatelessWidget {
                     color: Colors.white,
                   ),
                 ),
-
               ],
             ),
           ),
           Expanded(
             child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20.0),
-                  topRight: Radius.circular(20.0),
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0),
+                  ),
                 ),
-              ),
-
-            ),
+                child: const TasksList ()),
+                child:  const TasksList ()),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class TasksList extends StatelessWidget {
+  const TasksList ({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        TaskTile(),
+        TaskTile(),
+      ],
+    );
+  }
+}
+
+class TaskTile extends StatelessWidget {
+  const TaskTile({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: const Text('This is a task.'),
+      trailing: Checkbox(
+        value: false,
+        onChanged: (bool? value) {},
       ),
     );
   }
